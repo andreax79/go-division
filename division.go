765cd2819c3a01ea9014ddd5779376af66896e66
Andrea Bonomi <andrea.bonomi@gmail.com>
2023-01-25 11:01:58 +0100

    Division strut

--- division.go
+++ division.go
@@ -7,6 +7,25 @@ import (
 	"strings"
 )
 
+const empty = -1
+const separator = -2
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func spaces(n int) string {
+	if n > 0 {
+		return strings.Repeat(" ", n)
+	} else {
+		return ""
+	}
+}
+
 // Get the number of digits of a given number
 func NumberOfDigits(n int) int {
 	if n < 10 {
@@ -49,73 +68,116 @@ type Step struct {
 	left  int
 	right int
 	i     int
-}
-
-func spaces(n int) string {
-	return strings.Repeat(" ", n)
+	step  int
 }
 
 // Print a step
-func (step *Step) PrintStep(digits int) {
+func (step *Step) PrintStep(division *Division) {
+	left := ""
 	right := ""
-	if step.right >= 0 {
-		right = fmt.Sprint(step.right)
+	switch step.left {
+	case empty:
+		left = spaces(division.dividendDigits + 2)
+	case separator:
+		length := NumberOfDigits(step.i)
+		preSpaces := spaces(step.i - length + 2)
+		left = preSpaces + strings.Repeat("-", division.dividendDigits-step.i+1)
+	default:
+		length := NumberOfDigits(step.left)
+		preSpaces := spaces(step.i - length + 2)
+		postSpaces := spaces(division.dividendDigits - step.i)
+		left = fmt.Sprintf("%s%d%s", preSpaces, step.left, postSpaces)
 	}
-	fmt.Println(spaces(step.i-NumberOfDigits(step.left)+1), step.left, spaces(digits-step.i-1), "| ", right)
+	switch step.right {
+	case empty:
+		right = ""
+	case separator:
+		length := max(division.dividendDigits, NumberOfDigits(division.Result))
+		right = strings.Repeat("-", length)
+	default:
+		right = fmt.Sprintf(" %d", step.right)
+	}
+	fmt.Printf("%s|%s\n", left, right)
 }
 
-type Steps struct {
-	steps  []Step
-	digits int
+type Division struct {
+	steps          []Step
+	Dividend       int
+	Divisor        int
+	Result         int
+	Remainder      int
+	dividendDigits int
+	divisorDigits  int
+}
+
+// Create a new Division instance
+func NewDivision(dividend, divisor int) *Division {
+	division := new(Division)
+	division.steps = make([]Step, 0)
+	division.Dividend = dividend
+	division.Divisor = divisor
+	division.dividendDigits = NumberOfDigits(dividend)
+	division.divisorDigits = NumberOfDigits(divisor)
+	return division
+}
+
+// Calculate the division
+func (division *Division) Calculate() {
+	result := 0
+	remainder := 0
+	step := 0
+	division.addStep(division.Dividend, division.Divisor, division.dividendDigits-1, 0)
+	for i, ch := range fmt.Sprint(division.Dividend) {
+		n := int(ch - '0')
+		remainder = remainder*10 + n
+
+		if remainder >= division.Divisor {
+			if step > 0 {
+				division.addStep(remainder, empty, i, step)
+			}
+			d := 0
+			for remainder >= division.Divisor {
+				remainder = remainder - division.Divisor
+				d++
+			}
+			result = result*10 + d
+			division.addStep(d*division.Divisor, empty, i, step)
+			division.addStep(separator, empty, i, step)
+			step++
+		}
+	}
+	division.addStep(remainder, empty, division.dividendDigits-1, step)
+	division.setResult(result, remainder)
 }
 
 // Add a step
-func (steps *Steps) AddStep(left int, right int, i int) {
-	steps.steps = append(steps.steps, Step{left, right, i})
+func (division *Division) addStep(left int, right int, i int, step int) {
+	division.steps = append(division.steps, Step{left, right, i, step})
 }
 
-// Set result
-func (steps *Steps) SetResult(result int) {
-	steps.steps[1].right = result
-}
-
-// Print steps
-func (steps *Steps) Print() {
-	for _, step := range steps.steps {
-		step.PrintStep(steps.digits)
+// Set result and reminder
+func (division *Division) setResult(result, remainder int) {
+	division.Result = result
+	division.Remainder = remainder
+	division.steps[1].right = separator
+	if len(division.steps) < 3 {
+		division.addStep(empty, result, division.dividendDigits-1, 1)
+	} else {
+		division.steps[2].right = result
 	}
+}
+
+// Print the division
+func (division *Division) Print() {
+	for _, step := range division.steps {
+		step.PrintStep(division)
+	}
+	fmt.Printf("\n%d : %d = %d (%d)\n", division.Dividend, division.Divisor, division.Result, division.Remainder)
 }
 
 func main() {
 	dividend, divisor := ParseArgs(os.Args)
-
-	digits := NumberOfDigits(dividend)
-	result := 0
-	num := 0
-	steps := Steps{make([]Step, 0), digits}
-
-	steps.AddStep(dividend, divisor, digits-1)
-
-	for i, ch := range fmt.Sprint(dividend) {
-		n := int(ch - '0')
-		num = num*10 + n
-
-		if num >= divisor {
-			steps.AddStep(num, -1, i)
-			d := 0
-			for num >= divisor {
-				num = num - divisor
-				d++
-			}
-			result = result*10 + d
-			steps.AddStep(d*divisor, -1, i)
-		}
-	}
-	steps.AddStep(num, -1, digits-1)
-	steps.SetResult(result)
-	steps.Print()
-
-	fmt.Println()
-	fmt.Println("Result:", result)
-	fmt.Println("Remainder:", num)
+	division := NewDivision(dividend, divisor)
+	division.Calculate()
+	division.Print()
 }
